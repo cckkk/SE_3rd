3902d2465652d4ae9511abab3b43dab507b96c67
gitTestKNHR <leehosoo2@naver.com>
2015-11-04 19:04:52 +0900

    한번 날아가서 늦었습니다. 구구단 입니다. 단 마다 한 줄 넘어갑니다.
    프린트안에 계산이 들어가있는 형식입니다. (for문 사용)

--- 3rd/src/driver/MainDr.java
+++ 3rd/src/driver/MainDr.java
@@ -4,7 +4,20 @@ public class MainDr {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			
+		printGUGUDAN();
 	}
-
+	
+	
+	static void printGUGUDAN(){
+		
+		for(int i = 2; i < 10; i++){
+			for(int j = 1; j < 10; j++){
+				
+				System.out.print(i + " * " + j + " = " + i*j + "  ");
+			}
+			System.out.println();
+		}
+		return ;
+	}
 }
