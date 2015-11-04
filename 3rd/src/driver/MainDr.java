410e106b8e1624fc7271b12de587ad74a1e3f695
callab <callab@ENI-B9-14>
2015-11-04 19:03:31 +0900

    draw diamond입니다

--- 3rd/src/driver/MainDr.java
+++ 3rd/src/driver/MainDr.java
@@ -4,6 +4,19 @@ public class MainDr {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
+		drawDiamond();
+		
+	}
+	
+	public static void drawDiamond()
+	{
+		 System.out.println("  *  ");
+		 System.out.println(" *** ");
+		 System.out.println("*****");
+		 System.out.println(" *** ");
+		 System.out.println("  * ");
+
 
 	}
 
